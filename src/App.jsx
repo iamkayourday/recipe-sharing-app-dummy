03a964c434a119fbd32b336203f30158732cc7f7
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-08-24 20:24:26 +0100

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
