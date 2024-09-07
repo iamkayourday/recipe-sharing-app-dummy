16176c698f71e5d7264976a55f96b81019034b89
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-07 15:38:16 +0100

    check

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import { useRecipeStore } from './recipeStore';
-
-// const FavoritesList = ({ onToggleFavorite }) => {
-//   // Retrieve the list of favorite recipe IDs and find the corresponding recipes
-//   const { favorites = [], recipes = [] } = useRecipeStore(state => ({
-//     favorites: state.favorites,
-//     recipes: state.recipes,
-//   }));
-
-//   // Find the favorite recipes
-//   const favoriteRecipes = favorites.map(id => recipes.find(recipe => recipe.id === id)).filter(recipe => recipe !== undefined);
-
-//   return (
-//     <div>
-//       <h2>My Favorites</h2>
-//       {favoriteRecipes.length > 0 ? (
-//         favoriteRecipes.map(recipe => (
-//           <div key={recipe.id}>
-//             <h3>{recipe.title}</h3>
-//             <p>{recipe.description}</p>
-//             <button onClick={() => onToggleFavorite(recipe.id, true)}>Remove from Favorites</button>
-//           </div>
-//         ))
-//       ) : (
-//         <p>No favorite recipes added yet.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default FavoritesList;
 import React from 'react';
 import { useRecipeStore } from './recipeStore';
 
