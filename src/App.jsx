00697bfe1b106d7191ecd26e495a6ec1c81127a9
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-09-07 10:23:58 +0100

    Unnecessary components removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
-import SearchBar from './components/SearchBar';
-import FavoritesList from './components/FavoritesList';
-import RecommendationsList from './components/RecommendationsList';
+
 
 const App = () => {
   return (
@@ -15,14 +12,10 @@ const App = () => {
           textAlign:'center',
           color:'#646cff'
         }}>Recipe Sharing Application</h1>
-        {/* <SearchBar /> */}
-        {/* <AddRecipeForm /> */}
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
-        {/* <FavoritesList />
-        <RecommendationsList /> */}
       </div>
     </Router>
   );
